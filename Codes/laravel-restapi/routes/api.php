88d3e5a5933397d631f023b872559afe7e3bddca
ferdouszislam <ferdouszislam@gmail.com>
2022-01-11 17:32:57 +0600

    update api.php to map to ProductController

--- Codes/laravel-restapi/routes/api.php
+++ Codes/laravel-restapi/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,21 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function(){
-    
-    return Product::all();
-});
+Route::get('/products', [ProductController::class, 'index']);
 
+// Route::post('/products', function(){
 
-Route::post('/products', function(){
-
-    return Product::create([
-        'name' => 'Product One',
-        'slug' => 'product-one',
-        'description' => 'Product one created for testing.',
-        'price' => '0.00'
-    ]);
-});
+//     return Product::create([
+//         'name' => 'Product One',
+//         'slug' => 'product-one',
+//         'description' => 'Product one created for testing.',
+//         'price' => '0.00'
+//     ]);
+// });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
