33dbb5414901b90fa269bde7cab5a3e761146ddc
ferdouszislam <ferdouszislam@gmail.com>
2022-01-11 17:26:55 +0600

    updated Product model specifying fields and table name

--- Codes/laravel-restapi/app/Models/Product.php
+++ Codes/laravel-restapi/app/Models/Product.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    // table name
+    protected $table = 'Products'; 
+
+    // fields
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price'
+    ];
 }
