7ce53b69dda2d4ee3f0afc5c6457956a312e5ca6
ferdouszislam <ferdouszislam@gmail.com>
2022-01-11 21:57:44 +0600

    automatic changes while installing sanctum

--- Codes/laravel-restapi/vendor/composer/autoload_static.php
+++ Codes/laravel-restapi/vendor/composer/autoload_static.php
@@ -519,6 +519,7 @@ class ComposerStaticInitc7a1334e71b54677888c7fe1bd583dcd
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\ProductController' => __DIR__ . '/../..' . '/app/Http/Controllers/ProductController.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
