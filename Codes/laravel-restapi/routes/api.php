ce04e2166c1494fd123713e0cad0062d5bb87d97
ferdouszislam <ferdouszislam@gmail.com>
2022-01-11 22:11:02 +0600

    added sanctum authentication to routes

--- Codes/laravel-restapi/routes/api.php
+++ Codes/laravel-restapi/routes/api.php
@@ -15,13 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// routing to each api end point function separately
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
 
 // routing to a basic CRUD controller end points at once
-Route::resource('products', ProductController::class);
+// Route::resource('products', ProductController::class);
+
+// route to get all products
+Route::get('/products', [ProductController::class, 'index']);
+// route to get product by id
+Route::get('/products/{id}', [ProductController::class, 'show']);
+// route for search product by name
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+// routes with sanctum authentication enabled
+Route::group(['middleware' => 'auth:sanctum'], function() {
+
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'delete']);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function() {
+   
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
