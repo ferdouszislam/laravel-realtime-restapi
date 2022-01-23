2fbe0ac973cf4e1089bf9c6b3690e44dec60fa45
ferdouszislam <ferdouszislam@gmail.com>
2022-01-24 03:31:37 +0600

    update Message event and controller with model

--- Codes/laravel-restapi/app/Http/Controllers/MessagesController.php
+++ Codes/laravel-restapi/app/Http/Controllers/MessagesController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Events\MessageCreated;
 use App\Events\MessageSent;
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class MessagesController extends Controller
 {
@@ -15,8 +17,30 @@ class MessagesController extends Controller
 
     protected function sendMessage(Request $request)
     {
-        $message = $request->message;
+
+        // do not allow empty messages
+        $request->validate([
+            'content' => 'required'
+        ]);
+
+        $author = "anonymous";
+        $content = $request->content;
+
+        // check if author exists in request
+        if($request->has('author')) $author = $request->author;
+
+        $message = new Message([
+            'author' => $author,
+            'content' => $content
+        ]);
+
+        $message->save();
+
+        // send broadcast for realtime functionality
         broadcast(new MessageSent($message));
+
+        Log::info("sending: ". $message->toJson(JSON_PRETTY_PRINT));
+
         return response()->json([
             'status' => 'message is sent successfuly!'
         ]);
